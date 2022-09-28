36d940d9b798b82b5efdb12adb82aa3478403d73
Golam Rabbi Hasan <114388633+golamrabbi77@users.noreply.github.com>
2022-09-28 16:01:43 +0600

    Create Filp.m

---
+++ LAB-1/Filp.m
@@ -0,0 +1,18 @@
+clc;
+clear all;
+close all;
+
+a = imread('cat_image.jpg');
+b =rgb2gray(a);
+[r,c]=size(b);
+
+for i=1:1:r
+    k=1;
+    for j=c:-1:1
+        result(i+1,k)=b(r-i+1,j);
+        k=k+1;
+    end
+end   
+
+subplot(1,2,1),imshow(b);
+subplot(1,2,2),imshow(result);
